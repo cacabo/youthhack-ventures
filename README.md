5fe431cfdef8879c244066d73e45eb694081d98e
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-19 10:06:25 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## YouthHack Ventures
+New website for the student-run incubator's new 3 step plan to building business
+and entrepreneurial culture alike.
---
+++ js/script.js
@@ -0,0 +1,5 @@
+// When the document has finished loading...
+$(document).ready(function() {
+    // Set the logo's height to that of the brand text
+    $('.logo').outerHeight($('nav a').outerHeight());
+});
